e67594683022edc480387e5b1eff83f0d46edd8e
Jon1213 <jon1213@gmail.com>
2017-03-26 17:06:57 -0700

    I think Ive worked out a proper graph generation, including proper linking

--- problem3_continued/grid_graph.js
+++ problem3_continued/grid_graph.js
@@ -1,16 +1,6 @@
-function rand(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
 function Node(value, position, links){
-	this.position = position; // Integer 0 <= i < n^2. Integer values are much easier to compare than arrays, or even object values.
-	// Therefore, grids will be labeled left-to-right, then top to bottom, in ascending order.
-	// ex position representation for a 3x3 grid:
-	// 0,1,2
-	// 3,4,5
-	// 6,7,8
-
 	this.value = value; //a whole integer 0..9
+	this.position = position; // x,y dimensions. this allows us to create edges programmatically. upper left will have coordinates 1,1, lower right will have coordinates n,n
 	this.links = links; // [Node1, Node2,...] Nodes this node is linked to
 }
 
@@ -19,26 +9,59 @@ function Graph(n) {
 	this.nodes = [];
 }
 
+function rand(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 Node.prototype.addLink = function(node) {
-  this.links.push(node);
+  if(node !== null){
+  	this.links.push(node);
+  }
+};
+
+Node.prototype.getX = function(){
+	return this.position[0];
+};
+
+Node.prototype.getY = function(){
+	return this.position[1];
 };
 
 Graph.prototype.addNode = function(node){
 	this.nodes.push(node);
 };
 
+Graph.prototype.findNodeByCoordinates = function(x,y){
+	if(this.nodes( (x + y) - 1) >= Math.pow(this.dimensions, 2)){
+		return null;
+	}
+	return this.nodes[(x + y) - 1];
+};
+
 Graph.prototype.generate = function(n){
 	var grid_nodes = Math.pow(n, 2);
 	this.dimensions = n;
 
-	for(var i=0; i<grid_nodes;i++){
-		var node = new Node(rand(0, grid_nodes), i, []);
+	for(var i=0; i < grid_nodes; i++){
+		var x = (i % n) + 1;
+		var y = Math.floor(i / n) + 1;
+		var node = new Node(rand(0, grid_nodes), [x,y], []);
 		this.addNode(node);
 	}
 
+	// create directed links/edges without creating any cycles; pattern is: left-to-right for horizontal or diagonal, down for vertical
+	this.nodes.forEach(function(node){
+		var hor = this.getX();
+		var vert = this.getY();
 
-	// create paths
+		//if these don't exist, it simply won't add to the list
 
+		node.addLink(this.findNodeByCoordinates(hor+1, vert)); //node to the right
+		node.addLink(this.findNodeByCoordinates(hor+1, vert+1)); // node down 1 and right 1
+		node.addLink(this.findNodeByCoordinates(hor+1, vert-1)); // node up 1 and right 1
+		node.addLink(this.findNodeByCoordinates(hor, vert-1)); // node directly below
+
+	});
 };
 
 Graph.prototype.boggleSequences = function(){
